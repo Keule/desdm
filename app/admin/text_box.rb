e553875edb4b127535e116724165c763b096a59e
Prelang Builder <builder@prelang.com>
2016-02-19 13:34:00 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'TextBox' to allow for create, update

--- app/admin/text_box.rb
+++ app/admin/text_box.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register TextBox do
+  permit_params :top, :left, :height, :width, :font_size
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
