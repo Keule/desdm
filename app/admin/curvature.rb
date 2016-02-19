a2f5a3c50fd6df9439d38657a4b81e72f9fb4c12
Prelang Builder <builder@prelang.com>
2016-02-19 13:34:09 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Curvature' to allow for create, update

--- app/admin/curvature.rb
+++ app/admin/curvature.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Curvature do
+  permit_params :mode, :radius, :angle_offset, :center_x, :center_y
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
