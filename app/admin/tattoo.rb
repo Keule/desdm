ba46d3a3895fa853ed82f067becaac006967d2a6
Prelang Builder <builder@prelang.com>
2016-02-19 13:34:32 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Tattoo' to allow for create, update

--- app/admin/tattoo.rb
+++ app/admin/tattoo.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Tattoo do
+  permit_params :name, :image_path
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
