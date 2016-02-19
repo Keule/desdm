58be7a9f6ac35cadec03e4a3e86321e8ae68aeca
Prelang Builder <builder@prelang.com>
2016-02-19 13:34:20 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'FontAlignment' to allow for create, update

--- app/admin/font_alignment.rb
+++ app/admin/font_alignment.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register FontAlignment do
+  permit_params :alignment
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
