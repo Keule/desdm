2b4a77cfb93420be5144c434c0ef15376a078e49
Prelang Builder <builder@prelang.com>
2016-02-19 13:34:04 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'CurvatureDirection' to allow for create, update

--- app/admin/curvature_direction.rb
+++ app/admin/curvature_direction.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register CurvatureDirection do
+  permit_params :direction
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
